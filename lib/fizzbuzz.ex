5a31c3eda73f09e1ae2982eed4e0b8d4fb699115
charlie-galb <galbraith.charlie@gmail.com>
2021-04-19 11:49:24 +0100

    Handle case for numbers divisible by 5

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -3,12 +3,17 @@ defmodule Fizzbuzz do
   def play(num) do 
     cond do
       is_fizz(num) -> "fizz"
+      is_buzz(num) -> "buzz"
       true -> num
     end
   end
 
   defp is_fizz(num) do
     rem(num, 3) === 0 
+  end
+
+  defp is_buzz(num) do 
+    rem(num, 5) === 0
   end
 
   def hello do
--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -11,4 +11,7 @@ defmodule FizzbuzzTest do
   test "Outputs 'fizz' if input is divisible by 3 but not five" do
     assert Fizzbuzz.play(3) == "fizz"
   end
+  test "Outputs 'buzz' if input is divisible by 5 but not 3" do
+    assert Fizzbuzz.play(5) == "buzz"
+  end
 end
